75ec8f4fe6a5d3d0fe79f550d9c0e46a588c3813
efrencodes <efrencodes@gmail.com>
2021-11-03 00:18:36 -0600

    :sparkles: Add error and loading

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -8,14 +8,15 @@ import CreateTodoButton from '../CreateTodoButton'
 import useLocalStorage from '../../customHooks/useLocalStorage'
 
 const App = () => {
-	const [todos, saveTodos] = useLocalStorage(
+	const { todos, saveTodos, loading, error } = useLocalStorage(
 		'TODOLIST',
 		JSON.stringify([{ text: 'Curso de php', completed: false }])
 	)
+
 	const [search, setSearch] = useState('')
 
-	const totalTodo = todos.length
-	const completedTodo = todos.filter((todo) => !!todo.completed).length
+	const totalTodo = todos?.length
+	const completedTodo = todos?.filter((todo) => !!todo.completed).length
 
 	const onCompletedItem = (text) => {
 		const index = todos.findIndex((todo) => todo.text === text)
@@ -43,6 +44,9 @@ const App = () => {
 		<>
 			<TodoCounter todo={totalTodo} completed={completedTodo} />
 			<TodoSearch search={search} setSearch={setSearch} />
+			{error && <p>No te preocupes, hubo un problema</p>}
+			{loading && <p>Cargando tareas....</p>}
+			{!loading && !todos?.length && <p>Agrega una nueva tarea</p>}
 			<TodoList>
 				{todos?.map(({ text }) => (
 					// eslint-disable-next-line arrow-body-style
--- src/customHooks/useLocalStorage.js
+++ src/customHooks/useLocalStorage.js
@@ -1,25 +1,48 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const useLocalStorage = (itemName, itemValue) => {
-	const LocalTodoList = localStorage.getItem(itemName)
-	let parsedTodoList
+	const [value, setValue] = useState(itemValue)
+	const [error, setError] = useState(false)
+	const [loading, setLoading] = useState(true)
 
-	if (!LocalTodoList) {
-		localStorage.setItem(itemName, itemValue)
-		parsedTodoList = []
-	} else {
-		parsedTodoList = JSON.parse(LocalTodoList)
-	}
+	useEffect(() => {
+		try {
+			setTimeout(() => {
+				const LocalTodoList = localStorage.getItem(itemName)
+				let parsedTodoList
 
-	const [value, setValue] = useState(parsedTodoList)
+				if (!LocalTodoList) {
+					localStorage.setItem(itemName, itemValue)
+					parsedTodoList = itemValue
+				} else {
+					parsedTodoList = JSON.parse(LocalTodoList)
+				}
+				setValue(parsedTodoList)
+				setLoading(false)
+			}, 2000)
+		} catch (e) {
+			console.error(e)
+			setError(true)
+		}
+	}, [])
 
 	const saveTodos = (newTodos) => {
 		// JSON.stringify convierte un json a string.
-		localStorage.setItem(itemName, JSON.stringify(newTodos))
-		setValue(newTodos)
+		try {
+			localStorage.setItem(itemName, JSON.stringify(newTodos))
+			setValue(newTodos)
+		} catch (e) {
+			console.error(e)
+			setError(true)
+		}
 	}
 
-	return [value, saveTodos]
+	return {
+		value,
+		saveTodos,
+		loading,
+		error
+	}
 }
 
 export default useLocalStorage
